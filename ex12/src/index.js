55f34b6efaf82b03c131d1b4e559567ab933eb7d
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-21 10:29:01 +0200

    Generate Random Whole Nu

---
+++ ex12/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+function randomInteger() {
+    var randomNumber = Math.floor(Math.random() * 30);
+    console.log(randomNumber);
+}
+randomInteger();
+// Only change code above this line
+module.exports = randomInteger;
