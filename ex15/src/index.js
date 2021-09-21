dd6d6980a115d4a2c9d639f903e6a32350e4e314
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-21 10:31:00 +0200

    parseInt with Radix

---
+++ ex15/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function convertToInt(str){
+    var number = parseInt(str, 16);
+    console.log(number);
+}
+convertToInt("BA");
+convertToInt("F1");
+convertToInt("JeffBezos");
+// Only change code above this line
+module.exports = convertToInt;
